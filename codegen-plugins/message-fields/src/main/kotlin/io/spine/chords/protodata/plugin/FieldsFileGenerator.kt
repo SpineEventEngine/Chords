c25da9cb2694f075690325b86c306527d2e64f97
Dmitry Pikhulya <dmitry.pikhulya@teamdev.com>
2024-09-06 17:27:53 +0300

    Use `MessageField` as a class (instead of an interface)

--- codegen-plugins/message-fields/src/main/kotlin/io/spine/chords/protodata/plugin/FieldsFileGenerator.kt
+++ codegen-plugins/message-fields/src/main/kotlin/io/spine/chords/protodata/plugin/FieldsFileGenerator.kt
@@ -377,7 +377,7 @@ public class FieldsFileGenerator(
         val builderType = validatingBuilderClassName.parameterizedBy(messageClass)
 
         return TypeSpec.classBuilder(className)
-            .(superType)
+            .superclass(superType)
             .addProperty(
                 PropertySpec.builder("name", stringType, PUBLIC, OVERRIDE)
                     .initializer("\"${field.name.value}\"")
