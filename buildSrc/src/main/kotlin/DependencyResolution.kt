ccfb2bce405081326fce086b870ebb3e4c0adeb4
Oleg-Melnik <oleg.melnik@teamdev.com>
2024-09-09 13:55:49 +0100

    Suppress `Flogger` deprecation.

--- buildSrc/src/main/kotlin/DependencyResolution.kt
+++ buildSrc/src/main/kotlin/DependencyResolution.kt
@@ -121,6 +121,7 @@ private fun ResolutionStrategy.forceTestDependencies() {
  * Forces transitive dependencies of 3rd party components that we don't use directly.
  */
 private fun ResolutionStrategy.forceTransitiveDependencies() {
+    @Suppress("DEPRECATION") // Force version of Flogger lib.
     force(
         Asm.lib,
         Asm.tree,
