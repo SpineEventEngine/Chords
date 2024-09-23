5f288b5b82bfb2219aad2a3d96353cd858a9027a
Oleg-Melnik <oleg.melnik@teamdev.com>
2024-09-23 12:21:00 +0100

    Remove unused dependencies.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -36,14 +36,6 @@ repositories {
 }
 
 /**
- * The version of Jackson used by `buildSrc`.
- *
- * Please keep this value in sync. with `io.spine.internal.dependency.Jackson.version`.
- * It's not a requirement, but would be good in terms of consistency.
- */
-val jacksonVersion = "2.13.4"
-
-/**
  * The version of the Kotlin Gradle plugin.
  *
  * Please check that this value matches one defined in
@@ -60,16 +52,6 @@ val kotlinVersion = "1.8.22"
 val guavaVersion = "32.1.2-jre"
 
 /**
- * The version of ErrorProne Gradle plugin.
- *
- * Please keep in sync. with [io.spine.internal.dependency.ErrorProne.GradlePlugin.version].
- *
- * @see <a href="https://github.com/tbroyer/gradle-errorprone-plugin/releases">
- *     Error Prone Gradle Plugin Releases</a>
- */
-val errorPronePluginVersion = "3.1.0"
-
-/**
  * The version of Protobuf Gradle Plugin.
  *
  * Please keep in sync. with [io.spine.internal.dependency.Protobuf.GradlePlugin.version].
@@ -78,23 +60,6 @@ val errorPronePluginVersion = "3.1.0"
  *     Protobuf Gradle Plugins Releases</a>
  */
 val protobufPluginVersion = "0.9.4"
-
-/**
- * The version of Detekt Gradle Plugin.
- *
- * @see <a href="https://github.com/detekt/detekt/releases">Detekt Releases</a>
- */
-val detektVersion = "1.23.0"
-
-/**
- * @see [io.spine.internal.dependency.Kotest]
- */
-val kotestJvmPluginVersion = "0.4.10"
-
-/**
- * @see [io.spine.internal.dependency.Kover]
- */
-val koverVersion = "0.7.2"
 
 configurations.all {
     resolutionStrategy {
@@ -122,22 +87,11 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 }
 
 dependencies {
-    //implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
-    //implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:$jacksonVersion")
-
     implementation("com.google.guava:guava:$guavaVersion")
-    implementation("net.ltgt.gradle:gradle-errorprone-plugin:${errorPronePluginVersion}")
 
     // Add explicit dependency to avoid warning on different Kotlin runtime versions.
     implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
 
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:$detektVersion")
     implementation("com.google.protobuf:protobuf-gradle-plugin:$protobufPluginVersion")
-
-    // https://github.com/srikanth-lingala/zip4j
-    //implementation("net.lingala.zip4j:zip4j:2.10.0")
-
-    implementation("io.kotest:kotest-gradle-plugin:$kotestJvmPluginVersion")
-    implementation("org.jetbrains.kotlinx:kover-gradle-plugin:$koverVersion")
 }
