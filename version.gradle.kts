f808d7fc18c6b24c1b4293d373c2133dd0ebdfde
Artem Semenov <artem.semenov@teamdev.com>
2024-10-09 11:15:58 +0300

    Bump version of the Gradle plugin —> `1.9.4`.

--- version.gradle.kts
+++ version.gradle.kts
@@ -41,4 +41,4 @@ val chordsVersion: String by extra("2.0.0-SNAPSHOT.26")
  *
  * Update this version if the `chordsVersion` is updated.
  */
-val gradlePluginVersion: String by extra("1.9.3")
+val gradlePluginVersion: String by extra("1.9.4")
